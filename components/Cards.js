d53f176df4c4fc7feb98364ee3169b92aa9052dc
Zakhar Menshikov <zm681986@mail.ru>
2023-08-20 18:18:37 +0500

    test

--- components/Cards.js
+++ components/Cards.js
@@ -10,10 +10,27 @@ function Cards() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
             <div className={styles.page}>
-                ggggg
+                <ul className='catalog__elements'>
+                    {
+                        catalog.map((card, index) => {
+                            return (
+                                <li className="elements__card" key={index}>
+                                    <img className="image" src={card.src} />
+                                    <div className="elements__description">
+                                        <p className="card__text">Диван</p>
+                                        <p className="card__text">{ }</p>
+                                    </div>
+                                    <button className="card__button" type="button">Посмотреть</button>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
             </div>
         </>
     )
 }
 
 export default Cards;
+
+
