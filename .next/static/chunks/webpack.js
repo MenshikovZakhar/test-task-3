7c5847eb0125de53cf2516f6bf3aaaf009dee6be
Zakhar Menshikov <zm681986@mail.ru>
2023-08-27 20:14:33 +0500

    test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5b2756a4f9bab603"; }
+/******/ 		__webpack_require__.h = function() { return "2a1fc8573b699126"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -3,47 +3,42 @@ import fetch from 'isomorphic-unfetch'
 import styles from "../styles/index.module.css";
 import Head from 'next/head'
 import Card from '../components/Card';
-export default class extends Component {
-  static async getInitialProps() {
-    const res = await fetch('http://localhost:4000/cards')
-    const cards = await res.json()
-    return { cards }
-  }
-  componentWillMount() {
-    this.setState({
-      cards: this.props.cards
-    })
-  }
 
-  render() {
-    return (
-      <>
-        <Head>
-          <title>Тестовое задание</title>
-          <meta charSet='utf-8' />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <div className={styles.page}>
-          <ul className={styles.catalog__elements}>
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:4000/cards')
 
-            {
-              this.state.cards.map((card, index) =>
-                <Card
-                  id={card.id}
-                  key={index}
-                  data={card} />)
-            }
-          </ul>
-        </div>
-      </>
-    )
+  const cards = await res.json()
+
+  return {
+    props: {
+      cards
+    }
   }
 }
 
 
+export default function Blog({ cards }) {
+  return (
+    <>
+      <Head>
+        <title>Тестовое задание</title>
+        <meta charSet='utf-8' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className={styles.page}>
+        <ul className={styles.catalog__elements}>
 
-
-
-
+          {
+            cards.map((card, index) =>
+              <Card
+                id={card.id}
+                key={index}
+                data={card} />)
+          }
+        </ul>
+      </div>
+    </>
+  )
+}
 
 
