dd8d6aadd9dd76e8b98e8903136c5800f733ffb5
Zakhar Menshikov <zm681986@mail.ru>
2023-08-28 22:09:34 +0500

    test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "715c85e6a3a9af1c"; }
+/******/ 		__webpack_require__.h = function() { return "834f26eb2ca79727"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2f1d5d0b3f7044ac"; }
+/******/ 	__webpack_require__.h = function() { return "54e3ce77debdab46"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
